7e3054451dd2bab3edbfd691789e04d142d712e2
James Wookey <j.wookey@bristol.ac.uk>
2013-08-08 11:27:49 +0100

    Added routine to package up pre-calculated splitting operators into the structural form required for later SiMMS processing.

---
+++ SiMMS/SiS_populate_SplitOps.m
@@ -0,0 +1,75 @@
+% SIS_POPULATE_SPLITOPS
+%
+% // Part of SiMMS - Simple Matlab Modelling of Splitting //
+%
+% Load pre-existing splitting operators into a SplitOps structural array  
+%
+% [SplitOps] = SiS_populate_SplitOps(Rays, tlag, fast)
+% Inputs:
+%    Rays     : structure containing a set of raypaths through the model (see,
+%               e.g, SiS_create_VRays).
+%    fast     : vector of fast directions
+%    tlag     : vector of delay times
+%
+% Outputs: 
+%    SplitOps : Structure containing the list of splitting operators, one per
+%               ray.  
+
+% Copyright (c) 2003-2012, James Wookey 
+% All rights reserved.
+% This software is distributed under the term of the BSD free software license.
+% See end of file for full license terms.
+
+function [SplitOps] = SiS_populate_SplitOps(Rays, tlag, fast)
+%
+%  
+%
+   nRay = length(Rays) ;
+   for iRay = 1:length(Rays)
+      
+      SplitOps(iRay).x = Rays(iRay).x(end) ;
+      SplitOps(iRay).y = Rays(iRay).y(end) ;      
+
+      SplitOps(iRay).tlag = tlag(iRay) ;
+      SplitOps(iRay).fast = fast(iRay) ;
+      
+   end
+return
+
+%-------------------------------------------------------------------------------
+%
+%  This software is distributed under the term of the BSD free software license.
+%
+%  Copyright:
+%     (c) 2003-2012, James Wookey, University of Bristol
+%
+%  All rights reserved.
+%
+%   Redistribution and use in source and binary forms, with or without
+%   modification, are permitted provided that the following conditions are met:
+%        
+%   * Redistributions of source code must retain the above copyright notice,
+%     this list of conditions and the following disclaimer.
+%        
+%   * Redistributions in binary form must reproduce the above copyright
+%     notice, this list of conditions and the following disclaimer in the
+%     documentation and/or other materials provided with the distribution.
+%     
+%   * Neither the name of the copyright holder nor the names of its
+%     contributors may be used to endorse or promote products derived from
+%     this software without specific prior written permission.
+%
+%
+%   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+%   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%
+%-------------------------------------------------------------------------------
